a91b87a15e1c063216222db3cd7930385eec6736
Robert Parker <robbybparker@gmail.com>
2024-03-28 15:09:28 -0600

    remove param sweep from alamo_flowsheet file

--- svi/auto_thermal_reformer/alamo_flowsheet.py
+++ svi/auto_thermal_reformer/alamo_flowsheet.py
@@ -410,9 +410,6 @@ def make_simulation_model(X, P, surrogate_fname=None):
     return m
 
 
-df = {'X':[], 'P':[], 'Termination':[], 'Time':[], 'Objective':[], 'Steam':[], 'Bypass Frac': [], 'CH4 Feed':[]}
-
-
 if __name__ == "__main__":
     """
     The optimization problem to solve is the following:
@@ -420,45 +417,20 @@ if __name__ == "__main__":
     its maximum N2 concentration is 0.3, the maximum reformer outlet temperature is 1200 K and 
     the maximum product temperature is 650 K.  
     """
-    #for X in [0.90,0.91,0.92,0.93,0.94,0.95,0.96,0.97]:
-    for X in [0.95,0.96,0.97]:
-        #for P in np.arange(1447379, 1947379, 70000):
-        for P in [1450000, 1650000, 1850000]:
-            try: 
-                m = create_instance(X, P)
-                # Does this need to be applied after creating the surrogate? Why?
-                initialize_alamo_atr_flowsheet(m)
-                m.fs.reformer_bypass.inlet.temperature.unfix()
-                m.fs.reformer_bypass.inlet.flow_mol.unfix()
+    X = 0.95
+    P = 1650000
+    argparser = config.get_argparser()
+    args = argparser.parse_args()
+    surrogate_fname = os.path.join(args.data_dir, DEFAULT_SURROGATE_FNAME)
+    m = create_instance(X, P, surrogate_fname=surrogate_fname)
+    # Does this need to be applied after creating the surrogate? Why?
+    initialize_alamo_atr_flowsheet(m)
+    m.fs.reformer_bypass.inlet.temperature.unfix()
+    m.fs.reformer_bypass.inlet.flow_mol.unfix()
 
-                solver = get_solver()
-                solver.options = {
-                    "tol": 1e-7,
-                    "max_iter": 300
-                }
-                timer = TicTocTimer()
-                timer.tic('starting timer')
-                results = solver.solve(m, tee=True)
-                dT = timer.toc('end')
-                df[list(df.keys())[0]].append(X)
-                df[list(df.keys())[1]].append(P)
-                df[list(df.keys())[2]].append(results.solver.termination_condition)
-                df[list(df.keys())[3]].append(dT)
-                df[list(df.keys())[4]].append(value(m.fs.product.mole_frac_comp[0, 'H2']))
-                df[list(df.keys())[5]].append(value(m.fs.reformer_mix.steam_inlet.flow_mol[0]))
-                #df[list(df.keys())[5]].append(value(m.fs.steam_feed.flow_mol[0]))
-                df[list(df.keys())[6]].append(value(m.fs.reformer_bypass.split_fraction[0, 'bypass_outlet']))
-                df[list(df.keys())[7]].append(value(m.fs.feed.outlet.flow_mol[0]))
-            except ValueError:
-                df[list(df.keys())[0]].append(X)
-                df[list(df.keys())[1]].append(P)
-                df[list(df.keys())[2]].append("ValueError")
-                df[list(df.keys())[3]].append(999)
-                df[list(df.keys())[4]].append(999)
-                df[list(df.keys())[5]].append(999)
-                df[list(df.keys())[6]].append(999)
-                df[list(df.keys())[7]].append(value(m.fs.feed.outlet.flow_mol[0]))
-                continue
-
-    df = pd.DataFrame(df)
-    df.to_csv("sweep_results_alamo.csv")
+    solver = config.get_optimization_solver()
+    solver.config.options["honor_original_bounds"] = "yes"
+    timer = TicTocTimer()
+    timer.tic('starting timer')
+    results = solver.solve(m, tee=True)
+    dT = timer.toc('end')
