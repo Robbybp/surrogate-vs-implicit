cf6009c3d3377000ba613224f129fb3423500285
Robert Parker <robbybparker@gmail.com>
2024-04-28 16:58:58 -0600

    improve surrogate error plots

--- svi/auto_thermal_reformer/plot_surrogate_error.py
+++ svi/auto_thermal_reformer/plot_surrogate_error.py
@@ -22,6 +22,7 @@
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib.patches import Patch
 import seaborn as sns
 import svi.auto_thermal_reformer.config as config
 
@@ -29,9 +30,13 @@ import svi.auto_thermal_reformer.config as config
 def plot_surrogate_error(
     df,
     legend=True,
-    title=None,
     show_training_bounds=False,
 ):
+    # TODO: Make this (as well as font.family) a global option set
+    # when we get the argparser? Or maybe just make this configurable from the
+    # command line with a global default?
+    # But the same font size might not be appropriate for all plots.
+    plt.rcParams["font.size"] = 20
     fig = plt.figure()
 
     conversion_list = list(sorted(set(df["X"])))
@@ -47,12 +52,43 @@ def plot_surrogate_error(
         for j in range(n_conversion)
     ]
 
+    cbar = legend
+    # "Maximum relative error" (Between reactor model and surrogate model, over
+    # all output values, for the intput values at which the optimization stopped)
+    cbar_kws = dict(label="Maximum relative error")
     ax = sns.heatmap(
         error_array,
+        cbar=cbar,
+        cbar_kws=cbar_kws,
+        square=True,
+        cmap="Reds",
     )
+    # Change "background color" used for missing data (where either full-space
+    # or surrogate model failed)
+    ax.set_facecolor("black")
 
-    if title is not None:
-        ax.set_title(title)
+    if legend:
+        w, h = fig.get_size_inches()
+        # Getting the legend to appear in the right location is really annoying...
+        fig.set_size_inches(1.5*w, h)
+        legend_handles = [Patch(color="black", label="Failed simulation")]
+        ax.legend(
+            handles=legend_handles,
+            #ncol=1,
+            handlelength=0.75,
+            bbox_to_anchor=(2.4, 1.0),
+            #loc="upper right",
+            #borderaxespad=0,
+        )
+
+    xtick_positions = [i+0.5 for i in range(n_pressure)]
+    ytick_positions = [i+0.5 for i in range(n_conversion)]
+    xtick_labels = ["%1.2f" % (p / 1e6) if i%2 else "" for i, p in enumerate(pressure_list)]
+    ytick_labels = ["%1.2f" % x if i%2 else "" for i, x in enumerate(conversion_list)]
+    ax.set_xticks(xtick_positions, labels=xtick_labels)
+    ax.set_yticks(ytick_positions, labels=ytick_labels, rotation=0)
+    ax.set_xlabel("Pressure (MPa)")
+    ax.set_ylabel("Conversion")
 
     plt.gca().invert_yaxis()
 
@@ -65,9 +101,13 @@ def main(args):
     fig, ax = plot_surrogate_error(
         df,
         legend=not args.no_legend,
-        title=args.title,
         show_training_bounds=args.show_training_bounds,
     )
+
+    if args.title is not None:
+        ax.set_title(args.title)
+
+    fig.tight_layout()
 
     if not args.no_save:
         if args.plot_fname is None:
