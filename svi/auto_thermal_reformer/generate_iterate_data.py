63c0f63b0cbd3ae7edd4752debaaf0cd2944caee
Robert Parker <robbybparker@gmail.com>
2024-06-12 14:45:54 -0600

    script to generate iterate data from a solve

---
+++ svi/auto_thermal_reformer/generate_iterate_data.py
@@ -0,0 +1,91 @@
+#  ___________________________________________________________________________
+#
+#  Surrogate vs. Implicit: Experiments comparing nonlinear optimization
+#  formulations
+#
+#  Copyright (c) 2023. Triad National Security, LLC. All rights reserved.
+#
+#  This program was produced under U.S. Government contract 89233218CNA000001
+#  for Los Alamos National Laboratory (LANL), which is operated by Triad
+#  National Security, LLC for the U.S. Department of Energy/National Nuclear
+#  Security Administration. All rights in the program are reserved by Triad
+#  National Security, LLC, and the U.S. Department of Energy/National Nuclear
+#  Security Administration. The Government is granted for itself and others
+#  acting on its behalf a nonexclusive, paid-up, irrevocable worldwide license
+#  in this material to reproduce, prepare derivative works, distribute copies
+#  to the public, perform publicly and display publicly, and to permit others
+#  to do so.
+#
+#  This software is distributed under the 3-clause BSD license.
+#  ___________________________________________________________________________
+
+import os
+import svi.auto_thermal_reformer.config as config
+from svi.cyipopt import FullStateCallback
+import pandas as pd
+
+
+def main(args):
+    if args.sample is None:
+        conversion = 0.94
+        pressure = 1550000.0
+    else:
+        xp_samples = config.get_parameter_samples(args)
+        conversion, pressure = xp_samples[args.sample]
+    m = config.CONSTRUCTOR_LOOKUP[args.model](conversion, pressure)
+    callback = FullStateCallback()
+    solver = config.get_optimization_solver(callback=callback)
+
+    results = solver.solve(m, tee=True)
+
+    if args.fname is None:
+        fname = f"{args.model}-iterates"
+        if args.sample is not None:
+            fname += f"-{args.sample}"
+        fname += ".csv"
+    else:
+        fname = args.fname
+    fpath = os.path.join(args.data_dir, fname)
+
+    iterate_data = dict(callback.iterate_data)
+    # TODO: Should the keys here be updated to indicate that they are primal
+    # values and residuals? Probably, but I'll deal with that later.
+    iterate_data.update(callback.primal_values)
+    iterate_data.update(callback.primal_residuals)
+    # Make sure nothing pathological happened, like we had a variable named "inf_du"
+    assert len(iterate_data) == (
+        len(callback.iterate_data) + len(callback.primal_values) + len(callback.primal_residuals)
+    )
+
+    df = pd.DataFrame(iterate_data)
+    if not args.no_save:
+        print(f"Saving iterate data to {fpath}")
+        df.to_csv(fpath)
+    else:
+        print("--no-save is set. Not saving iterate data")
+
+
+if __name__ == "__main__":
+    argparser = config.get_sweep_argparser()
+
+    argparser.add_argument(
+        "--model",
+        default="fullspace",
+        help="Options are 'fullspace', 'implcit', 'alamo', or 'nn-full'. Default is 'fullspace'.",
+    )
+    argparser.add_argument(
+        "--fname",
+        default=None,
+        help="Basename for iterate data CSV file. Default is: {model}-iterates-{sample}.csv",
+    )
+    argparser.add_argument(
+        "--sample",
+        default=None,
+        type=int,
+        help="Index of conversion/pressure parameter sample to use. Default is X=0.95, P=1.55 MPa",
+    )
+
+    args = argparser.parse_args()
+    if args.subset is not None:
+        raise RuntimeError("--subset cannot be provided. Use --sample instead")
+    main(args)
