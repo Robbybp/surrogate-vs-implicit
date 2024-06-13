fab0cb1f985bd217a491000f7bc277ec2d5915cc
Robert Parker <robbybparker@gmail.com>
2024-06-13 15:26:36 -0600

    add consistent-call-signature constructors to config to support scripts that take a --model argument

--- svi/auto_thermal_reformer/config.py
+++ svi/auto_thermal_reformer/config.py
@@ -23,6 +23,60 @@ import argparse
 import itertools
 import pyomo.environ as pyo
 from svi.cyipopt import TimedPyomoCyIpoptSolver, Callback
+from svi.external import add_external_function_libraries_to_environment
+
+
+"""Consistent-signature callbacks to construct each model
+
+All models returned are initialized and ready to solve.
+
+Note that these constructors use local imports to avoid circular dependencies.
+The right solution here is probably to separate the single-model simulation drivers
+(which depend on config) from the model constructing "library" functions (which we
+need for these constructor callbacks).
+
+"""
+
+def fullspace_constructor(X, P, **kwds):
+    from svi.auto_thermal_reformer.fullspace_flowsheet import make_optimization_model as create_fullspace_instance
+    m = create_fullspace_instance(X, P)
+    return m
+
+
+def alamo_constructor(X, P, **kwds):
+    from svi.auto_thermal_reformer.alamo_flowsheet import (
+        create_instance as create_alamo_instance,
+        initialize_alamo_atr_flowsheet,
+    )
+    surrogate_fname = kwds.pop("surrogate_fname", None)
+    m = create_alamo_instance(X, P, surrogate_fname=surrogate_fname)
+    initialize_alamo_atr_flowsheet(m)
+    m.fs.reformer_bypass.inlet.temperature.unfix()
+    m.fs.reformer_bypass.inlet.flow_mol.unfix()
+    return m
+
+
+def nn_full_constructor(X, P, **kwds):
+    from svi.auto_thermal_reformer.nn_flowsheet import (
+        create_instance as create_nn_instance,
+        initialize_nn_atr_flowsheet,
+    )
+    surrogate_fname = kwds.pop("surrogate_fname", None)
+    # Note that KerasSurrogate.Formulation.FULL_SPACE is the default
+    m = create_nn_instance(X, P, surrogate_fname=surrogate_fname)
+    initialize_nn_atr_flowsheet(m)
+    m.fs.reformer_bypass.inlet.temperature.unfix()
+    m.fs.reformer_bypass.inlet.flow_mol.unfix()
+    return m
+
+
+def implicit_constructor(X, P, **kwds):
+    from svi.auto_thermal_reformer.fullspace_flowsheet import make_optimization_model as create_fullspace_instance
+    from svi.auto_thermal_reformer.implicit_flowsheet import make_implicit
+    m = create_fullspace_instance(X, P)
+    add_external_function_libraries_to_environment(m)
+    m_implicit = make_implicit(m)
+    return m_implicit
 
 
 filedir = os.path.dirname(__file__)
@@ -43,13 +97,22 @@ PARAM_SWEEP_KEYS = [
     'CH4 Feed',
 ]
 
-def get_optimization_solver(options=None, iters = 300):
+
+CONSTRUCTOR_LOOKUP = {
+    "fullspace": fullspace_constructor,
+    "implicit": implicit_constructor,
+    "alamo": alamo_constructor,
+    "nn-full": nn_full_constructor,
+}
+
+def get_optimization_solver(options=None, iters=300, callback=None):
     # Use cyipopt for everything for Ipopt version consistency among all
     # formulations
     #solver = pyo.SolverFactory("cyipopt")
     # This is a very simple callback we just use to get iteration counts.
-    cb = Callback()
-    solver = TimedPyomoCyIpoptSolver(intermediate_callback=cb)
+    if callback is None:
+        callback = Callback()
+    solver = TimedPyomoCyIpoptSolver(intermediate_callback=callback)
     if options is None:
         options = {}
     solver.config.options["max_iter"] = iters 
